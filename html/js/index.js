4a08e1a50deb8fb68258e60c9739bcb3670a0c6e
Monsterbolt101 <kerr34459@gmail.com>
2023-03-02 12:42:03 -0600

    Started work on dynamic problem set table.

--- html/js/index.js
+++ html/js/index.js
@@ -19,12 +19,7 @@ Vue.component("titleScreen", {
     `,
 });
 
-Vue.component("problemSetScreen", {
-    template: `
-    <div id="problemSet">
-        <h2>{{$root.problemSetGroup.title}}</h2>
-        <div id="problemSetTable">
-            <template v-for="category, i in $root.problemSetGroup.categories">
+{/* <template v-for="category, i in $root.problemSetGroup.categories">
                 <h3 style="text-align:left;">{{category.title}}</h3>
                 <div>
                     <template v-for="_, title in category.sets">
@@ -32,8 +27,15 @@ Vue.component("problemSetScreen", {
                     </template>
                 </div>
             </template>
-            <a :class="{'flow': true, 'active' : hasSelection}" @click="startQuestionSet()">Next >></a>
-        </div>
+            <a :class="{'flow': true, 'active' : hasSelection}" @click="startQuestionSet()">Next >></a> */}
+
+//<table id="problemSetTable"></table>
+
+Vue.component("problemSetScreen", {
+    template: `
+    <div id="problemSet">
+        <h2>{{$root.problemSetGroup.title}}</h2>
+        <div ref="problemSetTable"></div>
     </div>
     `,
     data() {
@@ -70,7 +72,67 @@ Vue.component("problemSetScreen", {
             this.active_set = "";
             this.$root.screenTransition(Screens.Question);
         },
+        buildTable() {
+            let self = this;
+            var table = this.$refs.problemSetTable;
+            table.innerHTML = ""; // Clear out previous children.
+            this.active_category = "";
+            this.active_set = "";
+
+            var row_height = 40;//Pixels
+            var column_width = 100;//Pixels
+            var row_count = Math.floor(table.clientHeight / row_height);
+            if(row_count == 0) {
+                console.warn("Row count is equal to zero!!!");
+                return;
+            }
+            console.log(`Row Height: ${row_height}; Client Height: ${table.clientHeight}; Row Count: ${row_count}`);
+            //row_count = 10;
+            var rows = [];
+            for(var i = 0; i < row_count; i++)
+                rows.push(table.appendChild(document.createElement("tr")));
+            var columns = 1;
+            var row = 0;
+            this.$root.problemSetGroup.categories.forEach((category, index)=>{
+                // Check to see if this category will be cutoff and give it the next row.
+                var element_count = Object.keys(category.sets).length;
+                if(element_count > row_count - row) {
+                    row = 0;
+                    columns++;
+                }
+                var title = rows[row++].appendChild(document.createElement("td"));
+                var text = title.appendChild(document.createElement("h3"));// = `<h3>${category.title}</h3>`;
+                text.innerHTML = category.title;
+                text.style["max-height"] = row_height + "px";
+                text.style["width"] = "100%";
+                text.style["text-align"] = "center";
+                Object.keys(category.sets).forEach((key)=>{
+                    // Check if we need to advance to the next row.
+                    if(row > row_count) {
+                        row = 0;
+                        columns++;
+                    }
+                    var element = rows[row++].appendChild(document.createElement("td"));
+                    var button = element.appendChild(document.createElement("button"));
+                    button.innerHTML = key;
+                    button.setAttribute("category", index);
+                    button.addEventListener('click', (event)=>{
+                        let btn = event.target;
+                        self.active_category = btn.getAttribute("category");
+                        self.active_set = btn.innerHTML;
+                    });
+                    button.style["max-height"] = row_height + "px";
+                    //button.style["width"] = "100%";
+                });
+            });
+        }
     },
+    // watch: {
+    //     "$refs.problemSetTable.clientHeight"() {
+    //         console.log("Height change!");
+    //         this.buildTable();
+    //     }
+    // }
 });
 
 Vue.component("vQuestion", {
@@ -175,6 +237,17 @@ const app = new Vue({
             // Select a random problem and pass it to the question screen.
             var i = Math.floor(Math.random() * newValue.length);
             this.$refs.question.question = newValue[i];
+        },
+        "problemSetGroup.categories"() {
+            console.log(this.$refs);
+            this.$refs.probleSets.buildTable();
+        },
+        screen(newValue) {
+            if(newValue == Screens.ProblemSet) {
+                Vue.nextTick(()=>{
+                    this.$refs.probleSets.buildTable();
+                }, this);
+            }
         },
     },
     created() {
